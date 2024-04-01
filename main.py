f44db56d59d6132ed1a5c5bae5f6a6bbdab70002
Luis080117 <luisgildardo1235@gmail.com>
2024-04-01 12:03:07 -0600

    Done

--- main.py
+++ main.py
@@ -2,13 +2,19 @@ from fastapi import FastAPI
 from routers import movie
 from routers import user
 from routers import login
+from typing import Union
 
 app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
 
 app.include_router(movie.router_movies)
 app.include_router(user.router_users)
 app.include_router(login.router_login)
 
 if __name__ == "__main__":
-    """import uvicorn
-    uvicorn.run("main:app", port=8000, reload=True)"""
+    import uvicorn
+    uvicorn.run("main:app", port=5000, reload=True, host="0.0.0.0")
--- routers/login.py
+++ routers/login.py
@@ -3,10 +3,14 @@ from models.user import User
 from services.login_service import user_login  # Assuming user_login is imported correctly
 from config.mongo_connection import connect_to_mongodb
 from schemas.user import User, UserCreate
+from fastapi import Header
 
 connection = connect_to_mongodb('test', 'users')
 router_login = APIRouter()
 
 @router_login.post('/login', tags=["Login"])
-def login(username: str = None, password: str = None, email: str = None, response: Response = None):
+def login(username: str = Header(None, description="User's username"),
+          password: str = Header(None, description="User's password"),
+          email: str = Header(None, description="User's email"),
+          response: Response = None):
     return user_login(username, password, email, response)
